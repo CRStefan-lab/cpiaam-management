84a22ff56e13d522ed4994f780c766d954c4abba
CRStefan-lab <chitu1robert1stefan@gmail.com>
2026-05-07 23:31:39 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// CPIAAM Service Worker — v13.34
-const CACHE_NAME = 'cpiaam-v13.34';
+// CPIAAM Service Worker — v13.35
+const CACHE_NAME = 'cpiaam-v13.35';
 const CDN_CACHE = 'cpiaam-cdn-v1';
 
 // CDN resources — cached permanently (versions pinned)
